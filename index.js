91a1133c8827e352ba74ffa448ae26e7f5409837
Camille (Cami) Johnson <74115479+Cami95@users.noreply.github.com>
2026-02-15 17:11:54 -0500

    added the js

---
+++ index.js
@@ -0,0 +1,8 @@
+function submitFullName() {
+    alert("My full name is ")
+}
+
+
+
+let fullNameElement = document.querySelector("#full-name")
+fullNameElement.addEventListener("submit", submitFullName)
